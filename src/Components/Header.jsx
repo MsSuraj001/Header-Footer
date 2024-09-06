9eee7fed8ad54fe103c16bd146aa3465a99d5125
MsSuraj001 <milukumar225@gmail.com>
2024-09-06 18:31:04 +0530

    uses the gsap with animation

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,33 @@
 import React from 'react'
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
 
 function Header() {
+    useGSAP( ()=>{
+        gsap.from('#box', {
+           y:-20,
+           duration: 1,
+           opacity:0,
+           delay: 0.5
+        })
+    })
+    useGSAP( ()=>{
+        gsap.from('#box2', {
+           x:40,
+           duration: 1,
+           opacity:0,
+           delay: 0.7,
+           scrub : 2
+        })
+    })
   return (
-    <div className='w-full h-[70px] bg-blue-900 px-16 flex justify-between font-semibold text-xl text-white'>
+    <div className='w-full h-[70px] bg-white shadow-lg  px-16 flex justify-between font-semibold text-2xl '>
       <div className='flex justify-center items-center'>
-        <h1>Ms Suraj</h1>
+        <h1 id='box'>Ms Suraj</h1>
       </div>
 
       <div className='flex justify-center items-center'>
-        <ul className='flex justify-center items-center gap-4'>
+        <ul id='box2' className='flex justify-center items-center gap-4'>
             <li className=''>Home </li>
             <li>About</li>
             <li>Service</li>
